b84e997d2c3c7719a4d40e80886795805aa90800
YoungJun <dudwns9073@nate.com>
2019-07-26 02:03:15 +0900

    익명함수 및 콜백 기능 구현

--- test/sync.js
+++ test/sync.js
@@ -1,9 +1,35 @@
 // Node.js - 동기와 비동기 그리고 콜백
 const fs = require('fs');
 
+// readFileSync
+/*
+console.log('A');
+var result = fs.readFileSync('./sample.txt', 'utf8');
+console.log(result);
+console.log('C');
+*/
+
+// sync + callback
 console.log('A');
 fs.readFile('../txt/sample.txt', 'utf-8', (err, result) => {
     console.log(result);
 });
-console.log('B');
+console.log('C');
 
+
+// first - class - citizen
+// function a() {
+//     console.log('A');
+// }
+
+
+// anoymous fucntion() 
+var a  = function() { 
+    console.log('A');
+}
+
+function slowfunc(callback) {
+    callback();
+}
+
+slowfunc(a);
