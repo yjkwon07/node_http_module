33c59c8292cf36e4cfab51234b77f2389d3183a9
YoungJun <dudwns9073@nate.com>
2019-07-26 11:50:32 +0900

    template 모듈화

--- main.js
+++ main.js
@@ -2,35 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
-
-const template = {
-    HTML: (title, list, body, control) => {
-        return `
-            <!doctype html>
-            <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                ${control}
-                ${body}
-            </body>
-            </html>
-        `;
-    },
-    List: (filelist) => {
-        let list = '<ul>';
-        for (let i = 0; i < filelist.length; i++) {
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list += '</ul>';
-        return list;
-    }
-};
-
+const template = require('./lib/template.js');
 
 const app = http.createServer((request, response) => {
     const _url = request.url;
